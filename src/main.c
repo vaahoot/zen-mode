16b176d5638e0af1de01882e27a6e18de64359eb
Volodymyr Vakhotskyi <vaahoot@gmail.com>
2026-04-20 13:51:07 +0100

    tool now runs as a process, unblocks everything when stopped

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include "hosts.h"
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,7 +7,7 @@
 
 void print_help(void) {
   printf("Usage: zen [options]\n");
-  printf("  -b, --block         block domains\n");
+  printf("  -b, --block         block domains (runs until interrupted)\n");
   printf("  -u, --unblock       unblock domains\n");
   printf("  -U, --unblock-all   remove all blocks\n");
   printf("  -h, --help          show this message\n");
@@ -14,6 +15,12 @@ void print_help(void) {
 
 int is_flag(const char *arg, const char *short_f, const char *long_f) {
   return strcmp(arg, short_f) == 0 || strcmp(arg, long_f) == 0;
+}
+
+static volatile sig_atomic_t stop_requested = 0;
+static void on_signal(int sig) {
+  (void)sig;
+  stop_requested = 1;
 }
 
 int main(int argc, char **argv) {
@@ -26,11 +33,20 @@ int main(int argc, char **argv) {
     return 0;
   }
 
+  const char **blocked = calloc((size_t)argc, sizeof(char *));
+  if (!blocked) {
+    fprintf(stderr, "Out of memory.\n");
+    return 1;
+  }
+  size_t blocked_count = 0;
+
   int had_failure = 0;
   for (int i = 1; i < argc; i++) {
     int (*action)(const char *) = NULL;
+    int is_block = 0;
     if (is_flag(argv[i], "-b", "--block")) {
       action = block_domain;
+      is_block = 1;
     } else if (is_flag(argv[i], "-u", "--unblock")) {
       action = unblock_domain;
     } else if (is_flag(argv[i], "-U", "--unblock-all")) {
@@ -39,25 +55,64 @@ int main(int argc, char **argv) {
       continue;
     } else if (is_flag(argv[i], "-h", "--help")) {
       print_help();
+      free(blocked);
       return 0;
     } else {
       fprintf(stderr, "Unknown argument: %s\n", argv[i]);
+      free(blocked);
       return 1;
     }
 
     const char *flag = argv[i];
     if (i + 1 >= argc || argv[i + 1][0] == '-') {
       fprintf(stderr, "Expected domains after %s\n", flag);
+      free(blocked);
       return 1;
     }
     while (i + 1 < argc && argv[i + 1][0] != '-') {
       i++;
-      if (action(argv[i]) != 0)
+      if (action(argv[i]) != 0) {
         had_failure = 1;
+      } else if (is_block) {
+        blocked[blocked_count++] = argv[i];
+      }
     }
   }
 
   flush_dns();
+
+  if (blocked_count == 0) {
+    free(blocked);
+    if (had_failure) {
+      fprintf(stderr, "Some operations failed.\n");
+      return 1;
+    }
+    printf("Done.\n");
+    return 0;
+  }
+
+  struct sigaction sa;
+  sa.sa_handler = on_signal;
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = 0;
+  sigaction(SIGINT, &sa, NULL);
+  sigaction(SIGTERM, &sa, NULL);
+  sigaction(SIGHUP, &sa, NULL);
+
+  printf("Blocking %zu domain(s). Press Ctrl-C to unblock and exit.\n",
+         blocked_count);
+
+  while (!stop_requested)
+    pause();
+
+  printf("\nUnblocking...\n");
+  for (size_t i = 0; i < blocked_count; i++) {
+    if (unblock_domain(blocked[i]) != 0)
+      had_failure = 1;
+  }
+  flush_dns();
+  free(blocked);
+
   if (had_failure) {
     fprintf(stderr, "Some operations failed.\n");
     return 1;
